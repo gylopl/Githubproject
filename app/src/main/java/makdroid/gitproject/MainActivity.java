1e2851318efb6910c7228a3579da9e4c01b2ab20
grzegorz_makowski <makowski.grzegorz.developer@gmail.com>
2016-06-23 21:32:32 +0200

    first commit

---
+++ app/src/main/java/makdroid/gitproject/MainActivity.java
@@ -0,0 +1,46 @@
+package makdroid.gitproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.editTextSearch)
+    EditText mEditTextSearch;
+    @Bind(R.id.recycler_view)
+    RecyclerView mRecyclerView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        initEditTextSearch();
+    }
+
+    void initEditTextSearch() {
+        mEditTextSearch.addTextChangedListener(searchWatcher);
+    }
+
+
+    private final TextWatcher searchWatcher = new TextWatcher() {
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+        }
+
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+}
